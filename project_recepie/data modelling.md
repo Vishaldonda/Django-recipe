8ef599d50f695e8fc6ad040a2343c3930c371815
Vishaldonda <vishal.d19@iiits.in>
2025-02-18 08:49:28 +0530

    Added static files

---
+++ project_recepie/data modelling.md
@@ -0,0 +1,1 @@
+First, what is a one-to-one relationship? It’s a relationship where a record in one entity (table) is associated with exactly one record in another entity (table).
--- project_recepie/project_recepie/settings.py
+++ project_recepie/project_recepie/settings.py
@@ -12,6 +12,18 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Ensure this matches your actual folder
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,7 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
