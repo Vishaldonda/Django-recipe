8b85203d4f29da6703f551461fbfd2816601af70
vishaldonda891 <vishalreckonsys@gmail.com>
2025-02-21 16:58:50 +0530

    Configured Docked Setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Django-recipe
+
+1. Build the image : `docker build -t my-recipe-app .`
+
+2. Run the Container :  `docker run -p 8000:8000 my-recipe-app`
+
+3. Access the server : `http://0.0.0.0:8000/`
--- project_recepie/project_recepie/settings.py
+++ project_recepie/project_recepie/settings.py
@@ -29,6 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
+
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -37,7 +38,7 @@ SECRET_KEY = 'django-insecure-g8vpq()3q%focir+w(txjv=gt2$=+eqg*k-i-dn=w+*zdgt=lk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
